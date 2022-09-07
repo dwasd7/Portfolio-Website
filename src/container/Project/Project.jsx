fed24c782c502204a69a65652305dd716a29aa11
Diwas <84002353+dwasd7@users.noreply.github.com>
2022-09-06 20:48:28 -0500

    Update Project.jsx

--- src/container/Project/Project.jsx
+++ src/container/Project/Project.jsx
@@ -12,34 +12,15 @@ const Project = () => {
           </h1>
         </div>
         <div className='Box'>
-            <h1>TicTacToe</h1>
+            <h1>RSC</h1>
             
             <div className='project_desc'>
-            <motion.img  whileHover={{scale:1.1 ,originY:.7, originX:0}} src={images.ttt} alt="" />
-             <p>TicTacToe game using Python’s default library which uses some basic structure to be able to create the board and rules for Two Players
-                    to be able to play the game </p>  
+            <motion.img  whileHover={{scale:1.1 ,originY:.7, originX:0}} src={images.rsc} alt="" />
+             <p>An Asssembly Program I created that is a Reletively Simple Calculator created through the use of Logic Gates and other Arithmetic Operations that preforms 16 Operations such as Loops and more</p>  
             </div>
             <motion.div whileHover={{scale:1.2}} className='button_edit'>
               <a href="
               https://github.com/dwasd7/TicTacToe
-              ">
-                <motion.button className ="button">
-                  Github
-                </motion.button>
-              </a>
-            </motion.div>
-        </div>
-
-        <div className='Box'>
-            <h1>Calculator</h1>
-            
-            <div className='project_desc'>
-            <motion.img  whileHover={{scale:1.1 ,originY:.7, originX:0}} src={images.calculator} alt="IMAGE NOT FOUND" />
-             <p>Simple Calculator Using the HTML, JAVA SCRIPT and CSS </p>  
-            </div>
-            <motion.div whileHover={{scale:1.1}} className='button_edit'>
-              <a href="
-              https://github.com/dwasd7/Simple-Calculator
               ">
                 <motion.button className ="button">
                   Github
@@ -65,11 +46,28 @@ const Project = () => {
             </motion.div>
         </div>
         <div className='Box'>
+            <h1>Calculator</h1>
+            
+            <div className='project_desc'>
+            <motion.img  whileHover={{scale:1.1 ,originY:.7, originX:0}} src={images.calculator} alt="IMAGE NOT FOUND" />
+             <p>Simple Calculator Using the HTML, JAVA SCRIPT and CSS </p>  
+            </div>
+            <motion.div whileHover={{scale:1.1}} className='button_edit'>
+              <a href="
+              https://github.com/dwasd7/Simple-Calculator
+              ">
+                <motion.button className ="button">
+                  Github
+                </motion.button>
+              </a>
+            </motion.div>
+        </div>
+        <div className='Box'>
             <h1>My Portfolio</h1>
             
             <div className='project_desc'>
             <motion.img  whileHover={{scale:1.1 ,originY:.7, originX:0}} src={images.portfolio} alt="IMAGE NOT FOUND" />
-             <p>My Portfolio Website using ReactJs and Css and other libraries like Framer-Motion for Animations </p>  
+             <p>My Portfolio Website using ReactJs and SCss and other libraries like Framer-Motion for Animations </p>  
             </div>
             <motion.div whileHover={{scale:1.1}} className='button_edit'>
               <a href="
